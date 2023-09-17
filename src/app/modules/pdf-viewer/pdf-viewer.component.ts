23f59802e7462c555d486fad609e7e80bfe71432
gorbo96 <46572873+gorbo96@users.noreply.github.com>
2023-09-17 13:48:27 -0500

    responsive changes

--- src/app/modules/pdf-viewer/pdf-viewer.component.ts
+++ src/app/modules/pdf-viewer/pdf-viewer.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-pdf-viewer',
   templateUrl: './pdf-viewer.component.html',
   styleUrls: ['./pdf-viewer.component.scss']
 })
-export class PdfViewerComponent {
-
+export class PdfViewerComponent implements OnInit{
+  page:number=1
+  zoom:number=1.0
+  pages:number=3
+  ngOnInit(): void {}
+  prevPage(){
+    if(this.page>1){
+      this.page--
+    }    
+  }
+  nextPage(){
+    if(this.page<this.pages){
+      this.page++
+    }
+  }
+  zoomIn(){
+    if(this.zoom<3.0){
+      this.zoom+=0.5
+    }
+  }
+  zoomOut(){
+    if(this.zoom>0.5){
+      this.zoom-=0.5
+    }
+  }   
 }
